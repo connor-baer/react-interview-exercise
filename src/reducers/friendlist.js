54100013d5e8a8d1ed85eb4a99854a14e0a1e740
Connor Bär <hello@connorbaer.co>
2017-05-08 17:00:03 +0200

    Updates ESLint config.

--- src/reducers/friendlist.js
+++ src/reducers/friendlist.js
@@ -36,14 +36,14 @@ export default function friends(state = initialState, action) {
             name: action.name
           }
         }
-      }
+      };
 
     case types.DELETE_FRIEND:
       return {
         ...state,
         friends: state.friends.filter(id => id !== action.id),
         friendsById: omit(state.friendsById, action.id)
-      }
+      };
 
     case types.STAR_FRIEND:
       return {
@@ -51,9 +51,9 @@ export default function friends(state = initialState, action) {
         friendsById: mapValues(state.friendsById, (friend) => {
           return friend.id === action.id ?
             assign({}, friend, { starred: !friend.starred }) :
-            friend
+            friend;
         })
-      }
+      };
 
     default:
       return state;
